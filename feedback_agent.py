12950095cfa53c2d20c575e93d6830e1fabda9f9
kngo44 <ktngo88@gmail.com>
2024-08-18 21:53:37 -0500

    Update feedback_agent.py

--- feedback_agent.py
+++ feedback_agent.py
@@ -1,3 +1,10 @@
 from langchain_openai import OpenAI
 from langchain.chains import SimpleChain
 from langchain.prompts import PromptTemplate
+
+def provide_feedback(user_responses, api_key):
+    llm = OpenAI(api_key=api_key, model="gpt-4")
+    prompt_template = PromptTemplate(
+        input_variables=["responses"],
+        template="Provide detailed feedback for the following interview responses:\n\n{responses}"
+    )
