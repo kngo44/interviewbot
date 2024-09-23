c891817cf64da4d9f5a617445de768f93447ce14
kngo44 <ktngo88@gmail.com>
2024-09-22 23:17:23 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,3 +5,4 @@ nlp = spacy.load('en_core_web_sm')
 def preprocess_text(text):
     doc = nlp(text)
     processed_text = " ".join([token.lemma_ for token in doc if not token.is_stop])
+    return processed_text
